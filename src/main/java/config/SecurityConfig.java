36023aaad2e577b9914ada980cb32cae09c840fa
“20it058 <20it058@charusat.edu.in>
2022-09-19 12:46:03 +0530

    Commit

---
+++ src/main/java/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("admin")
+                                .password("admin")
+                                .role("ADMIN")
+                );
+    }
+}
